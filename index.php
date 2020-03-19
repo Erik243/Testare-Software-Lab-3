2aeba5495791d0c17cc20001c829553fdb6a0f21
Erik243 <eric.semeniuc@ati.utm.md>
2020-03-19 22:20:50 +0200

    adauga index.php

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Pizza Shop</title>
+<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+<h1 style="text-align: center;">Pizza Shop</h1>
+
+<div class="topnav">
+  <a href="#index" class="active">Home</a>
+  <a href="ComPizza.php">Command Pizza</a>
+  <a href="Contact.php">Contact</a>
+ </div>
+
+ <div class="conteiner_prod" align="center" style=" border-style: ridge;">
+ 	<div class="pizza_prod" style="border-style: ridge;margin-top: 2%; margin-left: 31%; margin-right: 31%;">
+ 		<img src="img/Carciofini20ee.jpg" style="margin-top: 4px;">
+ 		<div style="border-style: ridge;margin-left: 1%; margin-right: 1%;background: rgb(204, 204, 204); background: rgba(204, 204, 204, 0.5);" >
+ 		<h2 id="title_pizza">Fresca</h2>
+ 		<p>2$</p>
+ 		<p>Greutate: 210 g .<br>Structură: Blat clasic/integral, sos pomodoro, mozzarella, șuncă, anghinare, ciuperci, măsline.</p>
+ 		<button style="height: 29px; width:  110px"><a href="ComPizza.php" >Comand</a></button>
+ 		</div>
+ 		
+ 	</div>
+ 	<div class="pizza_prod"style="border-style: ridge;margin-top: 2%;margin-left: 31%; margin-right: 31%;">
+ 		<img src="img/furiosa.jpg"style="margin-top: 4px; ">
+ 		<div style="border-style: ridge;margin-left: 1%; margin-right: 1%;background: rgb(204, 204, 204); background: rgba(204, 204, 204, 0.5);" >
+ 		<h2 id="title_pizza">Furiosa</h2>
+ 		<p>3$</p>
+ 		<p>Greutate: 250 g .<br>Structură: Blat clasic sau integral, sos pomodoro, mozzarella, salam, cîrnăciori vînătorești, ardei dulce, ardei iute</p>
+ 		<button style="height: 29px; width:  110px"><a href="ComPizza.php" >Comand</a></button>
+ 	</div>
+
+ 	</div>
+ 	<div class="pizza_prod"style="border-style: ridge;margin-top: 2%;margin-left: 31%; margin-right: 31%;">
+ 		<img src="img/gusto.jpg" style="margin-top: 4px;">
+ 		<div style="border-style: ridge;margin-left: 1%; margin-right: 1%;background: rgb(204, 204, 204); background: rgba(204, 204, 204, 0.5);" >
+ 		<h2 id="title_pizza">Gusto</h2>
+ 		<p>4$</p>
+ 		<p>Greutate: 280 g .<br>Structură: Blat classic/ integral, sos pomodoro, mozzarella, șuncă, ciuperci, măsline, busuioc, roșii.</p>
+ 		<button style="height: 29px; width:  110px"><a href="ComPizza.php" >Comand</a></button>
+ 	</div>
+ 	</div>
+<br><br>
+ </div>
+
+</body>
+</html>
+
